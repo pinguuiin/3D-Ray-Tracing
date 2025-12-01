b70c8e8c00d80eda6e291e1b686bfe4b270ea4c3
Yonatan Kadosh <167137372+conaffetto@users.noreply.github.com>
2025-12-01 18:38:28 +0200

    Changes the order of calls to 'preprocessor()' and 'initialize_mlx()', by making a slightly different instance of get_viewport_data()

--- src/main.c
+++ src/main.c
@@ -102,8 +102,8 @@ int	main(int argc, char *argv[])
 	parse_argument(argc, argv);
 	parse_scene(info, argv[1]);
 
-	initialize_mlx(info);
 	preprocessor(info);
+	initialize_mlx(info);
 
 	mlx_key_hook(info->mlx, &key_handler, info);
 	mlx_loop_hook(info->mlx, renderer, info);
@@ -122,8 +122,8 @@ int	main(int argc, char *argv[])
 	parse_argument(argc, argv);
 	parse_scene(info, argv[1]);
 
-	initialize_mlx(info);
 	preprocessor(info);
+	initialize_mlx(info);
 
 	initialize_multithreading(info);
 
