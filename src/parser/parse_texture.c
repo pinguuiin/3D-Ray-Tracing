4628234004238315a4544ce5853825b4e21635b3
ykadosh <167137372+conaffetto@users.noreply.github.com>
2025-12-10 18:30:38 +0200

    Cleans up parse_texture.c

--- src/parser/parse_texture.c
+++ src/parser/parse_texture.c
@@ -13,7 +13,7 @@
 #include "parser.h"
 
 #ifndef BONUS
-#else
+// #else // FIXME: uncomment this line.
 
 static size_t	strlen_texture_name(char *s);
 static int		allocate_texture_file_names(t_object *sphere, size_t len);
@@ -24,16 +24,11 @@ int	parse_texture_for_sphere(char **str, t_object *sphere, size_t line_num)
 {
 	size_t	len;
 
-	// parse axis vector
 	if (parse_3d_vector(str, &sphere->axis, line_num) == -1)
 		return (-1);
-
-	// set the z value of the sphere's axis to 0, and normalize the vector.
 	if (!validate_vector(&sphere->axis, line_num, SPHERE_AXIS))
 		return (INVALID_INPUT);
-
 	skip_whitespace_but_not_newline(str);
-
 	if (!**str || **str == '\n')
 	{
 		display_parsing_error("Unexpected texture input for sphere. If you'd "
@@ -42,23 +37,22 @@ int	parse_texture_for_sphere(char **str, t_object *sphere, size_t line_num)
 			"name (without the extension).\nError on line:", line_num);
 		return (INVALID_INPUT);
 	}
-
-	// parse string (texture file name)
 	len = strlen_texture_name(*str);
 	if (allocate_texture_file_names(sphere, len) == -1)
 		return (ALLOCATION_FAILURE);
 	prepare_tex_names(sphere, *str, len);
-
 	if (load_textures_and_free_them_upon_failure(sphere) == -1)
 		return (LOAD_TEXTURE_FAIL);
-
 	get_object_rot_matrix(sphere->rot, sphere->axis);
 	*str += len;
 	return (0);
 }
 
-// this will always be greater than 0, since there is a check for null terminator
-// and newline BEFORE calling it.
+/*
+* The length returned will always be greater than 0, since before calling this
+* function, there is a check for a newline and a null terminator. This is
+* important since that length will be used in order to allocate heap memory.
+*/
 static size_t	strlen_texture_name(char *s)
 {
 	size_t	i;
@@ -89,12 +83,17 @@ static void	prepare_tex_names(t_object *sphere, char *tex_name, size_t len)
 	ft_memmove(sphere->tex_file, "./textures/", 11);
 	ft_memmove(sphere->tex_file + 11, tex_name, len);
 	ft_memmove(sphere->tex_file + 11 + len, "_color.png", 10);
-
 	ft_memmove(sphere->normal_file, "./textures/", 11);
 	ft_memmove(sphere->normal_file + 11, tex_name, len);
 	ft_memmove(sphere->normal_file + 11 + len, "_normal.png", 11);
 }
 
+/*
+* Loads a texture via the MLX library, from both the color.png and normal.png
+* versions of the texture. If a normal.png file is not available for the,
+* texture, mlx_load_png() fails, but rendering still occurs, as miniRT handles
+* it gracefully.
+*/
 static int	load_textures_and_free_them_upon_failure(t_object *sphere)
 {
 	sphere->texture = mlx_load_png(sphere->tex_file);
@@ -108,8 +107,6 @@ static int	load_textures_and_free_them_upon_failure(t_object *sphere)
 		return (-1);
 	}
 	sphere->normal = mlx_load_png(sphere->normal_file);
-	// no need to handle the error, if loading the .png for sphere->normal has
-	// failed after this call, it is okay and we still render without it.
 	return (0);
 }
 #endif
