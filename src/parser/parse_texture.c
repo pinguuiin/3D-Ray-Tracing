ab57b0f3ba6d897f49b2cfc4c7776ceb0c9f59c3
Yonatan Kadosh <167137372+conaffetto@users.noreply.github.com>
2025-12-16 19:46:04 +0200

    Fixes issue in parsing of texture where no space was checked between axis vector and file name

--- src/parser/parse_texture.c
+++ src/parser/parse_texture.c
@@ -26,6 +26,8 @@ int	parse_texture_for_sphere(char **str, t_object *sphere, size_t line_num)
 
 	if (parse_3d_vector(str, &sphere->axis, line_num) == -1)
 		return (-1);
+	if (!is_valid_tail_when_expecting_more_data(str, line_num))
+		return (INVALID_INPUT);
 	if (!validate_vector(&sphere->axis, line_num, SPHERE_AXIS))
 		return (INVALID_INPUT);
 	skip_whitespace_but_not_newline(str);
